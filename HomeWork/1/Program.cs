d3a671b8a4c197037809cdd939e205b9eb442442
iliya2023 <ilyabury01071995@gmail.com>
2023-02-25 00:50:31 +0300

    HOMEWORK1

--- HomeWork/1/Program.cs
+++ HomeWork/1/Program.cs
@@ -27,7 +27,7 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-
+// 
 // Console.WriteLine("input first number ");
 // int num1 = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("input second number ");
